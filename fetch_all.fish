a3dff42ff3406371a1527282cdd49696d1e840cd
Dimitri Rusin <dimitri.rusin@smartfactory.de>
2021-04-06 16:51:05 +0200

    Fish script code to fetch commits in several git repositories.

---
+++ fetch_all.fish
@@ -0,0 +1,28 @@
+#!/bin/env fish
+
+cd ~/git/basyx/; and git fetch
+cd ~/git/code_from_keywords/; and git fetch
+cd ~/git/continuous_development/; and git fetch
+cd ~/git/deepspeech/; and git fetch
+cd ~/git/docker2root/; and git fetch
+cd ~/git/fabrikverwaltung/; and git fetch
+cd ~/git/fabrikverwaltung_backup/; and git fetch
+cd ~/git/gmaps/; and git fetch
+cd ~/git/howdoi/; and git fetch
+cd ~/git/ki4paul/; and git fetch
+cd ~/git/maps-platform-101-js/; and git fetch
+cd ~/git/markus.wico/; and git fetch
+cd ~/git/merge_log_files/; and git fetch
+cd ~/git/milo/; and git fetch
+cd ~/git/monitoring/; and git fetch
+cd ~/git/my-maven-app/; and git fetch
+cd ~/git/my-maven-app2/; and git fetch
+cd ~/git/pgjdbc/; and git fetch
+cd ~/git/softether-vpnclient-v4.34-9745-rtm-2020.04.05-linux-x64-64bit/; and git fetch
+cd ~/git/streamsheets/; and git fetch
+cd ~/git/sublime-selections/; and git fetch
+cd ~/git/tensorflow-test/; and git fetch
+cd ~/git/voice2json/; and git fetch
+cd ~/git/wico/; and git fetch
+cd ~/git/wordpress-docker/; and git fetch
+cd ~/git/youtube-stats/; and git fetch
